80085083be4d1c2f5eaed54844702bdaca2ee1b7
Myeonghee <myeonghee@danusys.com>
2023-02-24 15:04:37 +0900

    메인2

--- js/accodion.js
+++ js/accodion.js
@@ -1,40 +1,17 @@
 (()=>{
+    const accordionButtons = document.querySelectorAll('.side_submenu');
 
-    const accodionContainer = document.querySelector('.list_con');
-    let currentItem;
-
-    function activate(clickedItem) {
-        clickedItem.classList.add('active');
-        currentItem = clickedItem;
-    }
-
-    activate(document.querySelector('.list_con li:first-child'));
-
-    function inactivate(clickedItem){
-        clickedItem.classList.remove('active');
-    }
-
-    function accodionHandler(e){
-        const ct = e.target;
-        listCon = ct.closest('li');
-
-        if(ct.closest('ul') === accodionContainer) {
-            inactivate(currentItem);
-
-            if(ct.parentNode.getAttribute('data-roll') === "subMenuOpen"){
-                ct.parentNode.removeAttribute('data-roll', 'subMenuOpen');
-            }
-
-        }
-
-        if(ct.closest('ul') === accodionContainer) {
-            activate(ct.parentNode);
-            ct.parentNode.setAttribute('data-roll', 'subMenuOpen');
-        }
-
-       
-    }
-
-    accodionContainer.addEventListener('click', accodionHandler);
+    accordionButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+          this.classList.toggle('active');
+          console.log(this.nextElementSibling);
+          const accordionList = this.nextElementSibling;
+          if (accordionList.style.display === 'block') {
+            accordionList.style.display = 'none';
+          } else {
+            accordionList.style.display = 'block';
+          } 
+        });
+      });
 
 })();
