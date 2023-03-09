1ada8dbcc5515c0e83d79b92b5aa81b3f5a6ece5
Myeonghee <myeonghee@danusys.com>
2023-03-09 09:32:33 +0900

    컬러6

--- js/color.js
+++ js/color.js
@@ -13,47 +13,56 @@
      function inactivation(tab) {
          tab.classList.remove('active');
      }
+     
  
      function tabHandler(e) {
          const currentTab = e.target;
  
-         console.log(currentTab.parentNode);
-         console.log(checkElem);
  
          if(currentTab.parentNode === stickyArea1) {
-             inactivation(checkElem);
-             activation(currentTab);
+
+             if(currentTab === document.getElementById('hexTab')) {
+
+                inactivation(document.getElementById('rgbTab'));
+                activation(document.getElementById('hexTab'));
+
+                document.getElementById('hex').style.display = 'flex';
+                document.getElementById('rgb').style.display = 'none';
+             }
+    
+             if(currentTab === document.getElementById('rgbTab')) {
+
+                inactivation(document.getElementById('hexTab'));
+                activation(document.getElementById('rgbTab'));
+
+                document.getElementById('hex').style.display = 'none';
+                document.getElementById('rgb').style.display = 'flex';
+             }
  
-             console.log (checkElem);
          }
-         
-         if(currentTab === document.getElementById('hexTab')) {
-            document.getElementById('hex').style.display = 'flex';
-            document.getElementById('rgb').style.display = 'none';
-         }
-
-         if(currentTab === document.getElementById('rgbTab')) {
-            document.getElementById('hex').style.display = 'none';
-            document.getElementById('rgb').style.display = 'flex';
-         }
-
 
          if(currentTab.parentNode === stickyArea2) {
-            inactivation(checkElem);
-            activation(currentTab);
 
-            console.log (checkElem);
+            if(currentTab === document.getElementById('darkTab')) {
+
+                inactivation(document.getElementById('lightTab'));
+                activation(document.getElementById('darkTab'));
+
+                document.getElementById('darkTheme').style.display = 'flex';
+                document.getElementById('lightTheme').style.display = 'none';
+             }
+    
+             if(currentTab === document.getElementById('lightTab')) {
+
+                inactivation(document.getElementById('darkTab'));
+                activation(document.getElementById('lightTab'));
+
+                document.getElementById('darkTheme').style.display = 'none';
+                document.getElementById('lightTheme').style.display = 'flex';
+             }
+
         }
-
-        if(currentTab === document.getElementById('darkTab')) {
-            document.getElementById('darkTheme').style.display = 'flex';
-            document.getElementById('lightTheme').style.display = 'none';
-         }
-
-         if(currentTab === document.getElementById('lightTab')) {
-            document.getElementById('darkTheme').style.display = 'none';
-            document.getElementById('lightTheme').style.display = 'flex';
-         }
+         
      }
  
      activation(document.querySelector('#sticky1 button:nth-child(1)'));
